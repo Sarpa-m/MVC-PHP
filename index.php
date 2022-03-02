1cec6dd38b12771a04973d5c62de6f4f2af278cb
Maurício Sarpa <mausarpa02@gmai.com>
2022-03-02 15:11:53 -0300

    remove app e organiza index

--- index.php
+++ index.php
@@ -1,20 +1,58 @@
 <?php
 require_once __DIR__ . "/vendor/autoload.php";
+//CARRAA AS VARIAVES DE AMBIENTE
+WilliamCosta\DotEnv\Environment::load(__DIR__);
 
-require_once __DIR__ . "/Includes/app.php";
-
-
-
-
-
-
-//nicia o router
-//$obRouter = new Router(URL);
-
-// inclui as rotas
-require_once __DIR__ . "/router/pages.php";
-require_once __DIR__ . "/router/admin.php";
+use App\Http\Router;
+use App\Http\Middleware;
+use App\Http\Middleware\Queue as MiddlewareQueue;
+use App\Utils\View;
+use WilliamCosta\DatabaseManager\Database;
 
 
-//inprime o Response da Rota
+
+
+//DEFINE A URL BASE DO PROJETO
+define("URL", getenv("URL"));
+
+//INICIA O ROUTER
+$obRouter = new Router(URL);
+
+//CONFIGURA AS CLASS DE ROTA
+$obRouter->setRoutes([
+    Router\pages::class,
+    Router\admin::class,
+], $obRouter);
+
+//CONFIGURA O BANCO DE DADOS
+Database::config(
+    getenv("DB_HOST"),
+    getenv("DB_NAME"),
+    getenv("DB_USER"),
+    getenv("DB_PASS"),
+    getenv("DB_PORT")
+);
+
+//DEFINE O VALOR PADÃO DAS VARIAVES DA VIEW
+View::init([
+    "URL" => URL,
+    'URLc' => $obRouter->getcurrentUrl()
+]);
+
+//MAPEIA OS MIDDLEWARE
+MiddlewareQueue::setMap([
+    'mentenance' => Middleware\Mentenance::class
+]);
+
+//DEFINE OS MIDDLEWARES PADOES PARA TODAS AS ROTAS
+MiddlewareQueue::setDefault([
+    'mentenance'
+]);
+
+/* echo '<pre>';
+print_r($obRouter);
+echo '</pre>';
+exit; */
+
+//EXECUTA E INFRUME A ROTA
 $obRouter->run()->sendResponse();
